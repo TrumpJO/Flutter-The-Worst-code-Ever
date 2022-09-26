883d1ffb178a5a89618c471e6427fd7f65911a55
Ramzi Sayegh <sayegh.ramzi@yahoo.com>
2022-09-26 14:54:31 +0300

    fix theme value
    dark color should be pink & black, not pink & white.

--- lib/themes/theme_values.dart
+++ lib/themes/theme_values.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 class ThemeValues {
   static List<Color> brightColorsBackground = [Colors.pink, Colors.white];
-  static List<Color> darkColorsBackground = [Colors.pink, Colors.white];
+  static List<Color> darkColorsBackground = [Colors.pink, Colors.black];
 }
